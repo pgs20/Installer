b731504392fc0312e7f9bd0a72d789ef54b769f9
Gosha <gosha_petrov20@rambler.ru>
2024-01-11 21:28:00 +0300

    Доделал вторую часть задания (сохранение)

--- src/Installer.java
+++ src/Installer.java
@@ -1,4 +1,9 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Installer {
     public static void main(String[] args) {
@@ -21,7 +26,21 @@ public class Installer {
             fileWriter.close();
 
             // Сохранение
+            GameProgress gp1 = new GameProgress(100, 1, 99, 1.05);
+            GameProgress gp2 = new GameProgress(79, 3, 101, 1.65);
+            GameProgress gp3 = new GameProgress(90, 15, 150, 3.05);
 
+            String[] filesSave = {"\\savegames\\save1.dat", "\\savegames\\save2.dat", "\\savegames\\save3.dat"};
+            List<String> listFiles = new ArrayList<>(Arrays.asList("save1.dat", "save2.dat", "save3.dat"));
+            createFiles(filesSave, stringBuilder, path);
+
+            saveGame(path + filesSave[0], gp1);
+            saveGame(path + filesSave[1], gp2);
+            saveGame(path + filesSave[2], gp3);
+
+            createFiles(new String[] {"savegames\\zip.zip"}, stringBuilder, path);
+
+            zipFiles(path + "\\savegames\\zip.zip", listFiles);
 
         } catch (IOException e) {
             e.getMessage();
@@ -53,4 +72,20 @@ public class Installer {
         }
     }
 
+    public static void zipFiles(String pathToZip, List<String> files) {
+        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(pathToZip))) {
+            for (String nameFile : files) {
+                try(FileInputStream fis = new FileInputStream("C:\\Users\\79265\\learn-java\\tasksNetology\\Games\\savegames\\" + nameFile)) {
+                    ZipEntry entry = new ZipEntry("packed_" + nameFile);
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
